b0feb149adf63daf1a156401675356cbbb9a1fff
Sreejesh06 <sreejesh25122006@gmail.com>
2025-03-26 14:42:09 +0530

    feat: Implement draggable functionality for Floating TOC

--- floating-toc-extension/src/content.js
+++ floating-toc-extension/src/content.js
@@ -18,6 +18,9 @@ window.addEventListener('load', () => {
     </React.StrictMode>
   );
 
+  // Make TOC draggable
+  makeDraggable(container);
+
   // Add click handler to toggle TOC with keyboard shortcut
   document.addEventListener('keydown', (e) => {
     // Alt+T to toggle
@@ -26,6 +29,46 @@ window.addEventListener('load', () => {
     }
   });
 });
+
+// Function to make an element draggable
+function makeDraggable(element) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+  // Create drag handle
+  const dragHandle = document.createElement('div');
+  dragHandle.className = 'toc-drag-handle';
+  element.appendChild(dragHandle);
+  
+  dragHandle.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e.preventDefault();
+    // Get mouse position at startup
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e.preventDefault();
+    // Calculate new position
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // Set element's new position
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
+    element.style.right = 'auto';
+  }
+
+  function closeDragElement() {
+    // Stop moving when mouse button is released
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 // Function to toggle TOC visibility
 function toggleToc() {
