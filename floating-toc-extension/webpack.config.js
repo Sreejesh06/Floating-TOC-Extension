6a4fec162a960a1db237c64645c2f997c25d9076
Sreejesh06 <sreejesh25122006@gmail.com>
2025-03-26 11:05:04 +0530

    doc: Add webpack configuration for Floating TOC extension

---
+++ floating-toc-extension/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/content.js',
+  output: {
+    filename: 'content.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '.' }
+      ]
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
